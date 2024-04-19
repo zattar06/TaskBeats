753ea4983f0f174be2b74518b3995ef0c7685485
Gabriel Zattar <gabriel.zattar2000@gmail.com>
2024-04-18 21:59:44 -0300

    Initial commit

---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskDao.kt
@@ -0,0 +1,28 @@
+package com.comunidadedevspace.taskbeats
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(task: Task)
+
+    @Query("Select * from task")
+    fun getAll(): List<Task>
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun uptade(task: Task)
+
+    @Query("DELETE from task")
+    fun deleteAll()
+
+    @Query("DELETE from task WHERE id =:id")
+    fun deleteById(id: Int)
+
+}
